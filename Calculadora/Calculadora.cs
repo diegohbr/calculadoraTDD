411ed93ced000dba33f20e39bb7f69ff5fe010c8
diegohbr <diego.h.bittencourt@gmail.com>
2024-05-31 23:23:08 -0300

    solution commit

---
+++ Calculadora/Calculadora.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace NewTalents
+{
+    public class Calculadora
+    {
+        private List<string> listaHistorico;
+        private string _data;
+
+        public Calculadora(string data) 
+        {
+            listaHistorico = new List<string>();
+            _data = data;
+        }
+
+
+
+        public int somar(int val1, int val2)
+        {
+            int resultado = val1 + val2;
+
+            listaHistorico.Insert(0, "Res: " + resultado);
+            return resultado;
+        }
+
+        public int subtrair(int val1, int val2)
+        {
+            int resultado = val1 - val2;
+
+            listaHistorico.Insert(0, "Res: " + resultado);
+            return resultado;
+        }
+
+        public int multiplicar(int val1, int val2)
+        {
+            int resultado = val1 * val2;
+
+            listaHistorico.Insert(0, "Res: " + resultado);
+            return resultado;
+        }
+
+        public int dividir(int val1, int val2)
+        {
+            int resultado = val1 / val2;
+
+            listaHistorico.Insert(0, "Res: " + resultado);
+            return resultado;
+        }
+
+        public List<string> historico()
+        {
+            listaHistorico.RemoveRange(3, listaHistorico.Count -3);
+
+            return listaHistorico;
+        }
+    }
+}
